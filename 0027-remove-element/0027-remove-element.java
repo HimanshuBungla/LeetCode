92afd64b3c0924dbc0ec45e05d15fffe18ad3310
HimanshuBungla <84853089+HimanshuBungla@users.noreply.github.com>
2023-04-21 22:24:51 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (76.45%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -5,10 +5,9 @@ class Solution {
         // Loop through all the elements of the array
         for (int i = 0; i < nums.length; i++) {
             // If the element is not val
-            if (nums[i] == val) {
-                continue;
+            if (nums[i] != val) {
+                nums[count++] = nums[i];
             }
-            nums[count++] = nums[i];
         }
         return count;
     }
