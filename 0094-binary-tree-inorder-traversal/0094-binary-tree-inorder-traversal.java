804339aa101998a84547439d547a72e68a64d8a3
Himanshu Bungla <84853089+HimanshuBungla@users.noreply.github.com>
2023-05-19 09:53:36 +0530

    Time: 0 ms (100.00%), Space: 41 MB (19.21%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,15 +1,15 @@
 class Solution {
-    void rec(TreeNode root,List<Integer> ans)
+    void IOT(TreeNode root,List<Integer> ans)
     {
         if(root==null)
             return;
-        rec(root.left,ans);
+        IOT(root.left,ans);
         ans.add(root.val);
-        rec(root.right,ans);
+        IOT(root.right,ans);
     }
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer>ans= new ArrayList<>();
-        rec(root,ans);
+        IOT(root,ans);
         return ans;
     }
 }
