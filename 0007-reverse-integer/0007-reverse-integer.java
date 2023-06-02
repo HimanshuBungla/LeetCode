451f0b89276561b46447d890071f2dd95ed9f88b
Himanshu Bungla <84853089+HimanshuBungla@users.noreply.github.com>
2023-06-02 11:06:18 +0530

    Time: 1 ms (100.00%), Space: 39.5 MB (97.84%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,18 +1,18 @@
 class Solution {
     public int reverse(int x) {
-        boolean isNegative = false;
+        boolean n = false;
         if (x < 0) {
-            isNegative = true;
+            n = true;
             x = -x;
         }
-        long reverse = 0;
+        long rev = 0;
         while (x > 0) {
-            reverse = reverse * 10 + x % 10;
+            rev = rev * 10 + x % 10;
             x /= 10;
         }
-        if (reverse > Integer.MAX_VALUE) {
+        if (rev > Integer.MAX_VALUE) {
             return 0;
         }
-        return (int) (isNegative ? -reverse : reverse);
+        return (int) (n ? -rev : rev);
     }
 }
