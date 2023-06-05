78818d0346003cf58666ed53b6dcc9186be87731
Himanshu Bungla <84853089+HimanshuBungla@users.noreply.github.com>
2023-06-05 12:20:49 +0530

    Time: 1 ms (99.00%), Space: 40.7 MB (99.00%) - LeetHub

---
+++ 2129-capitalize-the-title/2129-capitalize-the-title.java
@@ -0,0 +1,19 @@
+class Solution {
+  public String capitalizeTitle(String title) {
+    StringBuilder sb = new StringBuilder(title.toLowerCase());
+
+    int i = 0; 
+    int j = 0; 
+
+    while (j < sb.length()) {
+      while (j < sb.length() && sb.charAt(j) != ' ')
+        ++j;
+      if (j - i > 2)
+        sb.setCharAt(i, Character.toUpperCase(sb.charAt(i)));
+      i = j + 1;
+      ++j; 
+    }
+
+    return sb.toString();
+  }
+}
