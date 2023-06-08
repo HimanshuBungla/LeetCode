5e4d8390b2913c606421c8d577b50746237be8ce
Himanshu Bungla <84853089+HimanshuBungla@users.noreply.github.com>
2023-06-08 12:00:34 +0530

    Time: 217 ms (15.32%), Space: 44.9 MB (5.39%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum1=0;
+        for(int i=0;i<nums.length;i++){
+            int sum2=0;
+            sum1=sum1+nums[i];
+            for(int j=i;j<nums.length;j++){
+                sum2 = sum2+nums[j];
+            }
+            if(sum1==sum2){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
