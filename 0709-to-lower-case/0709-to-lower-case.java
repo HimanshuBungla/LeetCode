61d3ab159e4dc4bcb24a9d7dac87e41510cfc7a2
Himanshu Bungla <84853089+HimanshuBungla@users.noreply.github.com>
2023-06-09 11:39:17 +0530

    Time: 1 ms (17.66%), Space: 40.8 MB (57.27%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -2,7 +2,7 @@ class Solution {
     public String toLowerCase(String s) {
         StringBuffer ans = new StringBuffer();
         for(int i=0;i<s.length();i++)
-          if(s.charAt(i)>=65 && s.charAt(i)<=90)
+          if(s.charAt(i)>='A' && s.charAt(i)<='Z')
               ans.append((char)(s.charAt(i) + (32)));
             else
               ans.append(s.charAt(i));
