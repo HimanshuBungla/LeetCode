6918ddfb069898c2518f47dc01438885e7395771
Himanshu Bungla <84853089+HimanshuBungla@users.noreply.github.com>
2023-07-03 09:55:04 +0530

    Time: 2 ms (82.29%), Space: 45.5 MB (71.20%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,18 +1,13 @@
 class Solution {
     public int[] replaceElements(int[] arr) {
-        for(int i=0;i<arr.length;i++){
-            int max =0;
-            if(i!=arr.length-1){
-                for(int j=i+1;j<arr.length;j++){
-                if(arr[j]>max){
-                    max=arr[j];
-                }
-            }
-            arr[i]= max;
-            }
-            else{
-                arr[i]=-1;
-            }
+        int n =arr.length;
+        int max = arr[n-1];
+        arr[n-1] = -1;
+        for(int i=n-2;i>=0;i--){
+            int temp = arr[i];
+            arr[i]=max;
+            if(max<temp)
+                max= temp;
         }
         return arr;
     }
