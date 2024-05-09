27b48e53e8f0081acc500299498b844f939a40e5
Himanshu Bungla <84853089+HimanshuBungla@users.noreply.github.com>
2024-05-09 10:52:46 +0530

    Time: 13 ms (97.67%), Space: 72.6 MB (84.71%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1637-widest-vertical-area-between-two-points-containing-no-points/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1637-widest-vertical-area-between-two-points-containing-no-points.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxWidthOfVerticalArea(int[][] points) {
+        int[] arr = new int[points.length];
+        for(int i=0;i<points.length;i++){
+            arr[i] = points[i][0];
+        }
+        
+        int res = 0;
+        Arrays.sort(arr);
+        for(int i=1;i<arr.length;i++){
+            res = Math.max(res,arr[i]-arr[i-1]);
+        }
+        
+        return res;
+    }
+}
