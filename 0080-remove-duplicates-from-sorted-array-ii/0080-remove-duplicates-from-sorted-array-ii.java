9a8ee635d1e2b2742fc5769c9278c6ca2943d5bf
Himanshu Bungla <84853089+HimanshuBungla@users.noreply.github.com>
2025-01-22 10:20:02 +0530

    Time: 0 ms (100%), Space: 44.7 MB (45.04%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,44 +1,15 @@
-// class Solution {
-//     public int removeDuplicates(int[] nums) {
-//         int j=0;
-//         int k=0;
-//         for(int i=1;i<nums.length;i++){
-//             if(nums[i]!=nums[j]&&k>=2){
-//                 j=j+2;
-//                 nums[j]=nums[i];
-//                 k=1;
-//             }
-//             else{
-//                 k++;
-//             }
-//         }
-//         return j+1;
-//     }
-// }
-
-
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int j = 0; // Pointer for the current position of allowed duplicates
-        int count = 1; // Counter to track the occurrences of the current number
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[j]) {
-                // Increment count if the current number is the same as the previous number
-                count++;
-            } else {
-                // Reset count when a new unique number is encountered
-                count = 1;
-            }
-
-            // If the count is within the allowed limit (2 in this case), update the array
-            if (count <= 2) {
+        if(nums.length<=2){
+            return nums.length;
+        }
+        int j=2;
+        for(int i=2;i<nums.length;i++){
+            if(nums[j-2]!=nums[i]){
+                nums[j]=nums[i];
                 j++;
-                nums[j] = nums[i];
             }
         }
-
-        // Return the length of the updated array
-        return j + 1;
+        return j;
     }
 }
