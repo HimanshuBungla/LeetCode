9ebcb99cee60893256a23592ed962ba7428ae5cb
Himanshu Bungla <84853089+HimanshuBungla@users.noreply.github.com>
2025-04-04 09:24:42 +0530

    Time: 19 ms (17.25%), Space: 14.7 MB (6.83%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,14 +1,18 @@
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        maj = nums[0]
-        count = 1
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        freq = 1
+        ans = nums[0]
         for i in range(1,len(nums)):
-            if(count==0):
-                count=count+1
-                maj = nums[i]
-            elif(nums[i]==maj):
-                count=count+1
+            if(ans==nums[i]):
+                freq +=1
             else:
-                count = count-1
-        return maj
-                
+                freq=1
+                ans = nums[i]
+            if(freq > len(nums)/2):
+                return ans
+        return ans
