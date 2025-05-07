2fc9f06cbca933e44c9abda77d755cce9a051eb3
Himanshu Bungla <84853089+HimanshuBungla@users.noreply.github.com>
2025-05-07 17:51:33 +0530

    Time: 3 ms (44.83%), Space: 18 MB (54.95%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.py
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.py
@@ -1,7 +1,11 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
-        ans = []
+        n = len(nums)
         for i in range(len(nums)):
-            ans.insert(i,nums[nums[i]])
-        return ans
+            nums[i] = nums[i] + n *(nums[nums[i]]%n)
+        
+        for i in range(len(nums)):
+            nums[i] = nums[i] //n
+        
+        return nums
         
