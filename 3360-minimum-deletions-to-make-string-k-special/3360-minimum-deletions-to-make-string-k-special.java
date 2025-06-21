3fe215953c745a7d27b94513fe852f9e5abd6a86
Himanshu Bungla <84853089+HimanshuBungla@users.noreply.github.com>
2025-06-21 11:28:42 +0530

    Time: 6 ms (51.06%), Space: 45.7 MB (34.04%) - LeetHub

--- 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.java
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.java
@@ -2,27 +2,28 @@ class Solution {
     public int minimumDeletions(String word, int k) {
         int[] freq = new int[26];
 
-        for (char ch : word.toCharArray()) {
-            freq[ch - 'a']++;
+        for(char ch : word.toCharArray()) {
+            freq[ch-'a']++;
         }
 
         Arrays.sort(freq);
 
-        int result = word.length();
-        int cumulativeDeleted = 0;
+        int result = Integer.MAX_VALUE;
+        int deletedTillNow = 0;
 
-        for (int i = 0; i < 26; i++) {
-            int del = cumulativeDeleted;
-            for (int j = 25; j > i; j--) {
-                if (freq[j] - freq[i] <= k) {
+        for(int i = 0; i < 26; i++) {
+            int minFreq = freq[i];
+            int temp = deletedTillNow; //temp taken to find deletion for j = 25 to j > i
+
+            for(int j = 25; j > i; j--) {
+                if(freq[j] - freq[i] <= k) 
                     break;
-                }
-
-                del += freq[j] - freq[i] - k;
+                
+                temp += freq[j] - minFreq - k;
             }
 
-            result = Math.min(result, del);
-            cumulativeDeleted += freq[i];
+            result = Math.min(result, temp);
+            deletedTillNow += minFreq;
         }
 
         return result;
